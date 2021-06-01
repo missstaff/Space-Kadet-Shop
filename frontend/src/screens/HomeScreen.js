87bc29eaf336d5cd66e366b65a4402c0b67f2e09
Shawna Staff <staffs@my.lanecc.edu>
2021-06-01 16:13:07 -0700

    fetching from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Product from "../components/Product.js";
-import data from "../data.js";
 
 export default function HomeScreen() {
+  /* React Hooks */
+  const [products, setProducts] = useState([]);
+
+  /* useEffect will run 1 time after didMount it takes 2 parameters: 1 function and an empty array for the dependencies*/
+  useEffect(() => {
+    /* fetchData is an async ajax request to backend server for data, defines fetchData */
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    /* calls fetchData */
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div className="row center">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <Product key={product._id} product={product}></Product>
         ))}
       </div>
