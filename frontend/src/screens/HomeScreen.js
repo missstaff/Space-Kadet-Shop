787b3713f28e20ed52fa2b4060dcacaaeebaf7d1
Shawna Staff <staffs@my.lanecc.edu>
2021-06-01 19:41:55 -0700

    products are served from data.js from backend server, created LoadingBox and MesaageBox components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,28 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import LoadingBox from "../components/LoadingBox.js";
+import MessageBox from "../components/MessageBox.js";
 import Product from "../components/Product.js";
 
 export default function HomeScreen() {
   /* React Hooks */
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false); //default value = false
+  const [error, setError] = useState(false);
 
   /* useEffect will run 1 time after didMount it takes 2 parameters: 1 function and an empty array for the dependencies*/
   useEffect(() => {
     /* fetchData is an async ajax request to backend server for data, defines fetchData */
     const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
     /* calls fetchData */
     fetchData();
@@ -19,11 +30,17 @@ export default function HomeScreen() {
 
   return (
     <div>
-      <div className="row center">
-        {products.map((product) => (
-          <Product key={product._id} product={product}></Product>
-        ))}
-      </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product}></Product>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
